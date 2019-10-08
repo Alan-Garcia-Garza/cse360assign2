b1f0aa922155f0d7a843ea115a4c300d557cca8a
alangarcia <alangarcia@192.168.0.9>
2019-10-08 00:11:01 -0700

    adding a main to test before using test cases

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -5,32 +5,43 @@
  * Assignment 2
  * This Assignment was created to help us know how to upload in 
  * a repository like github or bit.
+ * Adding program
  * 
  */
 package cse360assign2;
 
+//contains the function for the adding machine
 public class AddingMachine {
+	//keeps track of total value
 	private int total;
+	//keeps track of the string 
 	private String totalString;
 	
 	public static void main(String[] args) {
-		
+		AddingMachine addingMachine = new AddingMachine();
+		addingMachine.add(4);
+		addingMachine.subtract(2);
+		addingMachine.add(5);
+		//addingMachine.getTotal();
+		//addingMachine.toString();
+		System.out.print(addingMachine.toString()+"\n");
+		System.out.print(addingMachine.getTotal()+"\n");
 	}
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
-		totalString = "";
+		totalString = "0";
 	}
 	public int getTotal () {
 		return total;
 	}
 	
 	public void add (int value) {
-		total = + value;
+		total = total + value;
 		totalString = totalString + " + " + value;
 	}
 	public void subtract (int value) {
-		total = - value;
+		total = total - value;
 		totalString = totalString + " - " + value;
 	}
 	public String toString () {
@@ -38,6 +49,6 @@ public class AddingMachine {
 		}
 	public void clear() {
 		total=0;
-		totalString="";
+		totalString="0";
 	}
 }
