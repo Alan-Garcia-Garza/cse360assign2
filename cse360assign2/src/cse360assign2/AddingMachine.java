24a9cb31aabe540069d90f42a12f2e9e70ba471c
alangarcia <alangarcia@192.168.0.9>
2019-10-07 23:08:01 -0700

    Initial Commite

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Author: Alan Garcia Garza
+ * Class iD: 1212572164
+ * Assignment 2
+ * This Assignment was created to help us know how to upload in 
+ * a repository like github or bit.
+ * 
+ */
+package cse360assign2;
+
+public class AddingMachine {
+	private int total;
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	public void subtract (int value) {
+		
+	}
+	public String toString () {
+		return "";
+		}
+	public void clear() {
+		
+	}
+}
